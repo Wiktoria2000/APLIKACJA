1d7509ac1550e5317d85f3cb83ce3d4ebc8ff743
Katarzyna2000 <83282327+Katarzyna2000@users.noreply.github.com>
2021-06-11 21:46:16 +0200

    Update aplikacja.py

--- aplikacja.py
+++ aplikacja.py
@@ -201,3 +201,28 @@ if mapa == 'MP':
     x, y = m.gcpoints(Lat[0],Lon[0],Lat[1],Lon[1],500)
     plt.plot(x,y,color="#E59D59",linewidth=5)
     plt.show()
+if mapa == 'MS': 
+    lb = np.rad2deg(lB)
+    la = np.rad2deg(lA)
+    fa = np.rad2deg(fA)
+    fb = np.rad2deg(fB)       
+    plt.figure(figsize = (20,20))
+    h = 1000.
+    m = Basemap(projection='nsper',lon_0=(la+lb)/2,lat_0=(fa+fb)/2,
+        satellite_height=h*1000.,resolution='l')
+#m = Basemap(projection='merc',llcrnrlon=-15,llcrnrlat=35,urcrnrlon=69,
+            #urcrnrlat=72, lat_ts=0, resolution='l')
+#m.drawmapboundary(fill_color='#B5DAFE') #oceany
+#m.fillcontinents(color='#77C4A8',lake_color='#B5DAFE')
+#m.drawcountries(color='black',linewidth=1)
+#m.drawstates(linewidth = 0.2)
+#m.drawcoastlines()
+    m.bluemarble() #wygląd podobny do satelity ale bez granic państw
+    Points = {a:(fa,la),b:(fb,lb)}
+    Lon = [Points[key][0] for key in Points]
+    Lat = [Points[key][1] for key in Points]
+    X, Y = m(Lat,Lon)
+    m.scatter(X,Y,zorder=5,s=200,color="red",marker="^")
+    x, y = m.gcpoints(Lat[0],Lon[0],Lat[1],Lon[1],500)
+    plt.plot(x,y,color="#E59D59",linewidth=5)
+    plt.show()   
